06c570ee25447a6273e27b99ea20be47a66258fa
ketiil <33267921+ketiil@users.noreply.github.com>
2017-10-31 22:51:00 +0100

    quiz.py

---
+++ quiz.py
@@ -0,0 +1,60 @@
+from random import randint
+from os import system as execute
+from platform import system as checkOS
+
+class Quiz:
+
+
+
+    def __init__(self,):
+        self._question = []
+        self._answer = []
+        self._asked = []
+        #self._ordbok = {}
+        #self._teller = 0
+
+    def lesFraFil(self, filnavn):
+        quiz = open(filnavn, "r")
+        for i in quiz:
+            biter = i.split(";")
+            question = biter[0]
+            answer = biter[1].strip()
+            self._question.append(question)
+            self._answer.append(answer)
+            #self._ordbok[question] = answer
+
+
+    def getRandIndex(self):
+        x = randint(0, (len(self._question)-1))
+        return x
+
+    #Get question
+    def getQuestion(self):
+        i = self.getRandIndex()
+        self._asked.append(i)
+        return self._question[i]
+
+    #Print question
+    def printQuestion(self):
+        print(self.getQuestion())
+
+    #Get answer
+    def getAnswer(self):
+        i = self._asked[-1]
+        print(self._answer[i])
+
+    def dontShowAgain(self):
+        pass
+
+    def saveForLater(self):
+        pass
+
+    def kategorier(self):
+        pass
+
+    def clear_screen(self):
+        os = checkOS()
+        if os == "Windows":
+            execute("cls")
+        else:
+            execute("clear")
