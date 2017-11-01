ef4443acb00a832637a6ab06fab608d60284d8d9
ketiil <ketil.ellertsen@gmail.com>
2017-11-01 15:35:54 +0100

    Revamped main file. Much shorter.

--- main.py
+++ main.py
@@ -1,74 +1,11 @@
-from quiz import Quiz
+from menu import Menu
+
 
 def main():
 
-    #Oppretter objekt av Quiz
-    quiz = Quiz()
+    new_menu = Menu()
 
-    #Rydder skjermen
-    quiz.clear_screen()
-
-    #Mah loop
-    loop = True
-
-    #inpt = input("Input: ")
-    while loop:
-
-        #Hovedmeny
-        print("1: Start All Random \n")
-        print("2: Start Kategorier \n")
-        print("(Q)uit \n") #Should be consistent in menu
-        print("\n")
-        print("Andrea & Ketil (c) 2017")
-        print("\n")
-
-        inpt = input("Input: ")
-
-        #All Random
-        if inpt == '1':
-            quiz.clear_screen()
-            quiz.lesFraFil("questions.txt")
-            quiz.printQuestion()
-            #quiz.getQuestion()
-            while inpt != 'q':
-
-                #All Random meny
-                print("\n")
-                print("1: Vis svar")
-                print("2: Ta vare på til senere")
-                print("3: Neste")
-                print("4: Tilbake")
-                print("\n")
-
-                inpt = input("Input: ")
-                quiz.clear_screen()
-
-                #Get answer
-                if inpt == "1":
-                    quiz.getAnswer()
-                #Save for later
-                elif inpt == "2":
-                    pass
-                #Neste
-                elif inpt == "3":
-                    quiz.clear_screen()
-                    quiz.getAvailableQs()
-                    quiz.printQuestion()
-
-                #Return
-                elif inpt == "4":
-                    break
-
-        #Kategorier
-        elif inpt == 2:
-            quiz.clear_screen()
-            print("Kategorier: ")
-        #Quit
-        elif inpt.lower() == 'q':
-            loop = False
-        #Unknown input
-        else:
-            print("I didn't understand that. Please try again.")
+    new_menu.mainMenu()
 
 main()
 
